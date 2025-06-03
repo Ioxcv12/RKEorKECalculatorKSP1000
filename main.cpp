00cda14112264a381377fee75803a9dc6d22db33
Ioxcv12 <160195069+Ioxcv12@users.noreply.github.com>
2025-06-03 00:15:56 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,22 @@
 
 int main() {
     using namespace boost::multiprecision;
-    cpp_bin_float_100 M = 2600000000; //in kilograms
-    cpp_bin_float_100 V = 200075891.3; //velocity in m/s
-    cpp_bin_float_100 c = 1; //speed of light in c
-    //V *= 299792458; //remove the // at the start to calculate velocity in speed of lights
-    c *= 299792458;
+    std::cout << "Type mass here in kg:";
+    std::string MS;
+    std::getline(std::cin, MS);
+    cpp_bin_float_100 M(MS); //in kilograms
+
+    std::cout << "Type velocity here in m/s:";
+    std::getline(std::cin, MS);
+
+    cpp_bin_float_100 V(MS); //velocity in m/s
+
+    cpp_bin_float_100 c = 299792458; //speed of light in c
     cpp_bin_float_100 KER = (cpp_bin_float_100(1) / sqrt(cpp_bin_float_100(1) - (V * V) / (c * c)) - cpp_bin_float_100(1)) * M * c * c;
     cpp_bin_float_100 KE = cpp_bin_float_100(0.5) * (M * (V * V));
     std::cout << std::setprecision(100) << "Relativistic KE: " << KER << " J" << std::endl;
     std::cout << std::setprecision(100) << "Regular KE: " << KE << " J" << std::endl;
+    std::cout << "Press enter to exit";
+    std::cin.get();
     return 0;
 }
